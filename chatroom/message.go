7f8b29808edaf2a9316f9e9b45b888b0464d2dd0
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-06-01 23:40:04 +0530

    restructed chatroom user into domain

--- chatroom/message.go
+++
@@ -1,8 +0,0 @@
-package chatroom
-
-import "github.com/kabi175/chat-app-go/user"
-
-type Message struct {
-	Text     string
-	SenderId user.UserId
-}
--- user/send.go
+++
@@ -1,7 +0,0 @@
-package user
-
-var users map[UserId]*User
-
-func Ref(id UserId) *User {
-	return users[id]
-}
