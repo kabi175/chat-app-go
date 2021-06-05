68f9baf180538381f723a43e77a16aa33b39a302
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-06-05 14:49:13 +0530

    added jwt

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/kabi175/chat-app-go/server"
+import (
+	"github.com/kabi175/chat-app-go/http"
+	"github.com/kabi175/chat-app-go/repository"
+	"github.com/kabi175/chat-app-go/source"
+)
 
 func main() {
-	server := server.New()
-	server.Start("0.0.0.0:8000")
+	source := source.NewSource()
+	service := repository.NewService(&source)
+	handler := http.NewHandler(&service)
+	server := http.NewServer(&handler)
+	server.Config("0.0.0.0:8000")
+	server.Start()
 }
