d4da9749fc1ddf9095e2f41de5779fc0e58e43dd
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-08-18 14:39:45 +0530

    updated

--- messager-service/main.go
+++ messager-service/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -25,14 +24,9 @@ func main() {
 	router := inject()
 
 	server := http.Server{
-		Addr:        HOST,
-		Handler:     router,
-		ReadTimeout: 10 * time.Second,
+		Addr:    HOST,
+		Handler: router,
 	}
-
-	// Create and register close signal
-	closeSignal := make(chan os.Signal)
-	signal.Notify(closeSignal)
 
 	//Listen and serve go-routene
 	go func() {
@@ -42,7 +36,9 @@ func main() {
 			log.Fatalln(err)
 		}
 	}()
-
+	// Create and register close signal
+	closeSignal := make(chan os.Signal)
+	//signal.Notify(closeSignal)
 	// Wait for close signal, then gracefull shutdown server
 	<-closeSignal
 	ctx := context.Background()
