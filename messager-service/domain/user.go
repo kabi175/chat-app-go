8e399cc88a62900774d893c14d41bb187ab52fdb
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-12 10:58:55 +0530

    updated user

--- messager-service/domain/user.go
+++ messager-service/domain/user.go
@@ -1,18 +1,13 @@
 package domain
 
-import (
-	"github.com/gorilla/websocket"
-)
+import "gorm.io/gorm"
 
 type User struct {
-	UserName string
-	Conn     *websocket.Conn
-	Wait     chan struct{}
-}
-
-func NewUser(userName string) *User {
-	return &User{
-		UserName: userName,
-		Wait:     make(chan struct{}),
-	}
+	gorm.Model
+	ID          uint   `json:"id" gorm:"primarykey"`
+	DisplayName string `json:"displayName" gorm:"not null"`
+	Password    string `json:"password" gorm:"not null"`
+	Email       string `json:"email" gorm:"unique;not null"`
+	CreatedAt   int64  `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt   int64  `json:"updatedAt" gorm:"autoUpdateTime"`
 }
