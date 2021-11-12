5b11bdd671883cc812d0663e64dd60938c14447b
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-12 11:00:21 +0530

    add user repo port

--- messager-service/domain/repository_port.go
+++ messager-service/domain/repository_port.go
@@ -9,3 +9,11 @@ type UserStatusRepository interface {
 	Publish(*UserStatus) error
 	Listern(*User, chan UserStatus)
 }
+
+type UserRepo interface {
+	Create(*User) error
+	GetByID(uint) (*User, error)
+	GetByEmail(string) (*User, error)
+	DeleteByID(uint) error
+	DeleteByEmail(string) error
+}
