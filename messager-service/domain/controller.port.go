397adbc61d0d57225fa0f92d85c0bd4cbf57a94a
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-16 10:52:04 +0530

    add message controller

--- messager-service/domain/controller.port.go
+++ messager-service/domain/controller.port.go
@@ -1,10 +1,18 @@
 package domain
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type UserController interface {
 	Get(c *gin.Context)
 	Post(c *gin.Context)
 	Put(c *gin.Context)
 	Delete(c *gin.Context)
+}
+
+type MessageController interface {
+	Upgrade(http.ResponseWriter, *http.Request)
 }
