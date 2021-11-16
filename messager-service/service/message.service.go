fcd3414b435e85b4e08ed46a836ddb371fea84de
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-16 10:53:41 +0530

    add message service

--- messager-service/service/message.service.go
+++ messager-service/service/message.service.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"context"
+
+	"github.com/kabi175/chat-app-go/messager/domain"
+)
+
+type DefaultMessageService struct {
+	messageRepo domain.MessageRepo
+}
+
+func NewDefaultMessageService(messageRepo domain.MessageRepo) domain.MessageService {
+	return &DefaultMessageService{messageRepo: messageRepo}
+}
+
+func (s *DefaultMessageService) PublishMessage() func(*domain.Message) error {
+	return func(message *domain.Message) error {
+		err := s.messageRepo.Producer(message, context.Background())
+		return err
+	}
+}
+
+func (s *DefaultMessageService) ConsumeMessage(user *domain.User, ctx context.Context) (func() <-chan domain.MessageChan, error) {
+	msgChan, err := s.messageRepo.Consumer(user, ctx)
+	return func() <-chan domain.MessageChan { return msgChan }, err
+}
